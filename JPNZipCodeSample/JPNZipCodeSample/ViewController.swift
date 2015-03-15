2a2670de1617785e9e17c3a2b7a9d6cc6ab03b34
yutmr <yu.ampm@gmail.com>
2015-03-15 19:59:18 +0900

    initial commit

---
+++ JPNZipCodeSample/JPNZipCodeSample/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  JPNZipCodeSample
+//
+//  Created by YuTamura on 2015/03/15.
+//  Copyright (c) 2015年 YuTamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var textField: UITextField?
+    @IBOutlet var label: UILabel?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func searchAddress(sender: AnyObject) {
+        var zipCode = self.textField?.text
+        JPNZipCode.get(zipCode, completionHandler: { (response, error) -> Void in
+            if response != nil {
+                var status: NSNumber = response!["code"]! as NSNumber
+                
+                switch (status.compare(400)) {
+                    case .OrderedSame, .OrderedDescending:
+                        break;
+                    case .OrderedAscending:
+                        var address = response!["data"]!["fullAddress"]! as String
+                        dispatch_async(dispatch_get_main_queue(), {
+                            self.label!.text = address;
+                        })
+                        break;
+                }
+            }
+        })
+    }
+}
+
---
+++ Source/JPNZipCode.swift
@@ -0,0 +1,36 @@
+//
+//  JPNZipCode.swift
+//  JPNZipCodeSample
+//
+//  Created by YuTamura on 2015/03/15.
+//  Copyright (c) 2015年 YuTamura. All rights reserved.
+//
+
+import Foundation
+
+
+public class JPNZipCode {
+
+    class public func get(zipCode: String!, completionHandler: ((NSDictionary?, NSError?) -> Void)) {
+        var URL     = NSURL(string: "http://api.zipaddress.net?zipcode=" + zipCode)
+        var config  = NSURLSessionConfiguration.defaultSessionConfiguration()
+        var session = NSURLSession(configuration: config)
+        var task    = session.dataTaskWithURL(URL!, completionHandler: { (data, response, error) -> Void in
+            if error != nil {
+                completionHandler(nil, error)
+            }
+
+            var jsonError: NSError?
+            var json: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as NSDictionary
+            if jsonError == nil {
+                completionHandler(nil, jsonError)
+            }
+            
+            completionHandler(json, nil);
+        })
+        task.resume()
+    }
+}
+
+
+
